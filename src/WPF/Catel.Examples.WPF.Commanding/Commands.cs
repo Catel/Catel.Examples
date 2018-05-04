7fff383b2042a0b21ec649d294bc88b8ac13434b
philjpearson <philjpearson@gmail.com>
2018-05-04 15:49:27 +0100

    Update Commands.cs
    Add commands to demonstrate the problem reported in Issue 1185 : InputGesture not properly handled by CommandManager

--- src/WPF/Catel.Examples.WPF.Commanding/Commands.cs
+++ src/WPF/Catel.Examples.WPF.Commanding/Commands.cs
@@ -16,5 +16,10 @@ namespace Catel.Examples.Commanding
 
         public const string GlobalAction = "GlobalAction";
         public static readonly InputGesture GlobalActionInputGesture = new InputGesture(Key.G, ModifierKeys.Control);
+
+        public const string Test1 = "Test1";
+        public static readonly InputGesture Test1InputGesture = new InputGesture(Key.A, ModifierKeys.Control|ModifierKeys.Shift);
+        public const string Test2 = "Test2";
+        public static readonly InputGesture Test2InputGesture = new InputGesture(Key.A, ModifierKeys.Control|ModifierKeys.Alt);
     }
 }
