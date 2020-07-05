f76ca66adf24573b4ed43fd8009c139c2b2dcfa5
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-07-04 22:06:47 -0400

    Update Catel.Examples.XF.MasterDetail.UWP to latest Xamarin.Forms

--- src/Catel.Examples.XF.MasterDetail.UWP/MainPage.xaml.cs
+++ src/Catel.Examples.XF.MasterDetail.UWP/MainPage.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-namespace DemoApp2.UWP
+﻿namespace Catel.Examples.Xamarin.Forms.MasterDetail.UWP
 {
     public sealed partial class MainPage
     {
@@ -21,7 +6,7 @@ namespace DemoApp2.UWP
         {
             this.InitializeComponent();
 
-            LoadApplication(new DemoApp2.App());
+            LoadApplication(new App3.App());
         }
     }
 }
