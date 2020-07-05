1fe7763946e42e0d6c57884091ff36a74ebbdfd3
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2020-07-04 21:37:18 -0400

    Update Catel.Examples.XF.MasterDetail.iOS to latest Xamarin.Forms

--- src/Catel.Examples.XF.MasterDetail.iOS/Main.cs
+++ src/Catel.Examples.XF.MasterDetail.iOS/Main.cs
@@ -1,15 +1,15 @@
-﻿using UIKit;
-
-namespace DemoApp2.iOS
+﻿namespace Catel.Examples.XF.MasterDetail.iOS
 {
-	public class Application
-	{
-		// This is the main entry point of the application.
-		static void Main(string[] args)
-		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main(args, null, "AppDelegate");
-		}
-	}
+    using UIKit;
+
+    public class Application
+    {
+        // This is the main entry point of the application.
+        public static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
 }
