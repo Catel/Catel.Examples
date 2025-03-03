8c67b279356b8ec6ec928ec80116758176607a41
Geert van Horrik <geert@catenalogic.com>
2025-03-03 18:49:51 +0100

    More cleanup

--- src/MethodTimeLogger.cs
+++ src/MethodTimeLogger.cs
@@ -1,34 +1,40 @@
-using System.Reflection;
-using Catel.Logging;
-using System;
+// using System.Reflection;
+// using Catel.Logging;
+// using System;
 
-/// <summary>
-/// Note: do not rename this class or put it inside a namespace.
-/// </summary>
-internal static class MethodTimeLogger
-{
-    #region Methods
-    public static void Log(MethodBase methodBase, long milliseconds, string message)
-    {
-        Log(methodBase.DeclaringType, methodBase.Name, milliseconds, message);
-    }
+// /// <summary>
+// /// Note: do not rename this class or put it inside a namespace.
+// /// </summary>
+// internal static class MethodTimeLogger
+// {
+    // #region Methods
+    // public static void Log(MethodBase methodBase, long milliseconds, string message)
+    // {
+        // Log(methodBase.DeclaringType, methodBase.Name, milliseconds, message);
+    // }
 
-    public static void Log(Type type, string methodName, long milliseconds, string message)
-    {
-        if (type is null)
-        {
-            return;
-        }
+    // public static void Log(Type type, string methodName, long milliseconds, string message)
+    // {
+        // if (type is null)
+        // {
+            // return;
+        // }
 
-        var finalMessage = $"[METHODTIMER] {type.Name}.{methodName} took '{milliseconds}' ms";
+        // if (milliseconds == 0)
+        // {
+            // // Don't log superfast methods
+            // return;
+        // }
 
-        if (!string.IsNullOrWhiteSpace(message))
-        {
-            finalMessage += $" | {message}";
-        }
+        // var finalMessage = $"[METHODTIMER] {type.Name}.{methodName} took '{milliseconds.ToString()}' ms";
 
-        var logger = LogManager.GetLogger(type);
-        logger.Debug(finalMessage);
-    }
-    #endregion
-}
+        // if (!string.IsNullOrWhiteSpace(message))
+        // {
+            // finalMessage += $" | {message}";
+        // }
+
+        // var logger = LogManager.GetLogger(type);
+        // logger.Debug(finalMessage);
+    // }
+    // #endregion
+// }
