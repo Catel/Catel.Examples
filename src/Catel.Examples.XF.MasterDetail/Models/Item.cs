f32cc50d1b645f8788bf01ee351d06f32c63d964
Geert van Horrik <geert@catenalogic.com>
2025-03-03 18:39:42 +0100

    Remove obsolete projects

--- src/Catel.Examples.XF.MasterDetail/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Catel.Examples.Xamarin.Forms.MasterDetail.Models
-{
-    public class Item : DataObjectBase
-    {
-        public string Text { get; set; }
-
-        public string Description { get; set; }
-    }
-}
